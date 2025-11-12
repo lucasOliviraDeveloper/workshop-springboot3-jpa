909f6289be98ee59c3723a39caecfc7754fbb146
Lucas Oliveira <lucas.gabrielsilva2011@gmail.com>
2025-11-12 13:36:16 -0300

    Service layer, component registration

--- src/main/java/com/lucasdev/project/resources/UserResource.java
+++ src/main/java/com/lucasdev/project/resources/UserResource.java
@@ -1,20 +1,32 @@
 package com.lucasdev.project.resources;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.lucasdev.project.entities.User;
+import com.lucasdev.project.services.UserService;
 
 @RestController
 @RequestMapping(value = "/users")
 public class UserResource {
 
+	@Autowired
+	private UserService service;
 	@GetMapping
-	public ResponseEntity<User> findAll(){
-		User u = new User(1L,"Lucas","lucas.gabrielsilva2011@gmail.com","31992300329","12345");
-		return ResponseEntity.ok().body(u);
+	public ResponseEntity<List<User>> findAll(){
+		List<User> list = service.findAll();
+		return ResponseEntity.ok().body(list);
 	}
 	
+	@GetMapping("/{id}")
+	public ResponseEntity<User> findById(@PathVariable Long id){
+		User obj = service.findById(id);
+		return ResponseEntity.ok().body(obj);
+	}
 }
