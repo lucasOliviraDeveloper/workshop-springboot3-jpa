64bfe9f344228dfd845e44be2addf24bb351180e
Lucas Oliveira <lucas.gabrielsilva2011@gmail.com>
2025-11-22 18:59:55 -0300

    Exception handling - findById

---
+++ src/main/java/com/lucasdev/project/services/exceptions/ResourceNotFoundException.java
@@ -0,0 +1,10 @@
+package com.lucasdev.project.services.exceptions;
+
+public class ResourceNotFoundException extends RuntimeException{
+
+	private static final long serialVersionUID = 1L;
+
+	public ResourceNotFoundException(Object id) {
+		super("Resource not found. Id " + id );
+	}
+}
